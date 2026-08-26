4120780cc1d6f8d102d79ca191aa71936942c1f4
Ky-Wilson <yanne.kouassi@epitech.eu>
2026-08-26 10:23:56 +0000

    fix(seeders): renomme Membres LinkedIn en Membres, icone users

--- database/seeders/HomeStatSeeder.php
+++ database/seeders/HomeStatSeeder.php
@@ -11,7 +11,7 @@ class HomeStatSeeder extends Seeder
     {
         $stats = [
             ['icon' => 'fa-solid fa-users',           'label' => 'Members',          'value' => 0,    'suffix' => '+', 'auto_count' => true,  'model' => 'App\\Models\\User',     'order' => 0],
-            ['icon' => 'fa-brands fa-linkedin',       'label' => 'Membres LinkedIn', 'value' => 1100, 'suffix' => '+', 'auto_count' => false, 'model' => null,                    'order' => 1],
+            ['icon' => 'fa-solid fa-users',            'label' => 'Membres',          'value' => 1100, 'suffix' => '+', 'auto_count' => false, 'model' => null,                    'order' => 1],
             ['icon' => 'fa-brands fa-whatsapp',       'label' => 'Membres WhatsApp', 'value' => 420,  'suffix' => '+', 'auto_count' => false, 'model' => null,                    'order' => 2],
             ['icon' => 'fa-solid fa-calendar-check',  'label' => 'Événements',       'value' => 12,   'suffix' => '+', 'auto_count' => true,  'model' => 'App\\Models\\Event',    'order' => 3],
             ['icon' => 'fa-solid fa-book-open',       'label' => 'Articles',         'value' => 2,    'suffix' => '+', 'auto_count' => true,  'model' => 'App\\Models\\Article',  'order' => 4],
